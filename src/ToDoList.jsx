123502cd1805f03dbb19fadecfb546f8d6d5596b
Lokendra Nath <lokendraverma225@gmail.com>
2025-01-31 18:12:02 +0530

    initialize codes

---
+++ src/ToDoList.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { TaskItems } from "./components/TaskItems";
+import PropTypes from "prop-types";
+
+function ToDoList({ tasks, setTasks }) {
+  const [inputText, setInputText] = useState("");
+
+  function inputChange(event) {
+    setInputText(event.target.value);
+  }
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  }
+  function addTask() {
+    if (!inputText.trim()) {
+      alert("Please Enter a Task");
+      return;
+    }
+
+    const newTaskObj = {
+      id: crypto.randomUUID(),
+      name: inputText,
+      checked: false,
+    };
+
+    setTasks((t) => [newTaskObj, ...t]);
+    setInputText("");
+  }
+
+  return (
+    <div className="custom-task-container">
+      <h1 className="text-5xl font-bold text-center mb-11">To Do List</h1>
+      <div>
+        <input
+          type="text"
+          className="custom-input  focus:ring-2 focus:ring-green-500"
+          onChange={inputChange}
+          value={inputText}
+          placeholder="Enter a Task ..."
+          onKeyDown={handleKeyDown}
+        />
+        <button
+          className="px-3 h-12 font-medium ml-4 bg-green-600 rounded-[8px] text-[18px]"
+          onClick={addTask}
+        >
+          Add
+        </button>
+      </div>
+
+      <TaskItems tasks={tasks} setTasks={setTasks} />
+    </div>
+  );
+}
+
+ToDoList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      checked: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+  setTasks: PropTypes.func.isRequired,
+};
+
+export default ToDoList;
---
+++ src/components/TaskItems.jsx
@@ -0,0 +1,110 @@
+import {
+  ArrowBigUpDash,
+  ArrowBigDownDash,
+  Trash2Icon,
+  Circle,
+  CheckCircle,
+} from "lucide-react";
+import PropTypes from "prop-types";
+
+export function TaskItems({ tasks, setTasks }) {
+  function deleteTask(index) {
+    const updatedTask = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTask);
+  }
+
+  function handleIsChecked(id) {
+    setTasks((t) => {
+      const updatedTasks = t.map((task) =>
+        task.id === id ? { ...task, checked: !task.checked } : task
+      );
+      return [
+        ...updatedTasks.filter((task) => !task.checked),
+        ...updatedTasks.filter((task) => task.checked),
+      ];
+    });
+  }
+
+  function moveTask(index, direction) {
+    if (
+      (index > 0 && direction === "up") ||
+      (index < tasks.length - 1 && direction === "down")
+    ) {
+      const newIndex = direction === "up" ? index - 1 : index + 1;
+      const newTasks = [...tasks];
+      [newTasks[index], newTasks[newIndex]] = [
+        newTasks[newIndex],
+        newTasks[index],
+      ];
+      setTasks(newTasks);
+    }
+  }
+  return (
+    <>
+      <ul className="mt-[30px] w-[500px] text-center">
+        {tasks.map((task, index) => (
+          <li key={task.id} className="custom-li">
+            <div className="flex flex-1 gap-2 text-start">
+              <button
+                aria-label={task.checked ? "Uncheck task" : "Check task"}
+                onClick={() => handleIsChecked(task.id)}
+              >
+                {task.checked ? (
+                  <CheckCircle className="w-5" />
+                ) : (
+                  <Circle className="w-5" />
+                )}
+              </button>
+              <p
+                className={
+                  task.checked
+                    ? "custom-task-text line-through opacity-70"
+                    : "custom-task-text"
+                }
+              >
+                {task.name}
+              </p>
+            </div>
+            <div
+              className={
+                task.checked
+                  ? "custom-right-circle opacity-70"
+                  : "custom-right-circle"
+              }
+            >
+              <button
+                className="bg-green-400 custom-y active:-translate-y-1"
+                onClick={() => moveTask(index, "up")}
+              >
+                <ArrowBigUpDash className="w-5" />
+              </button>
+              <button
+                className="bg-yellow-400 custom-y active:translate-y-1"
+                onClick={() => moveTask(index, "down")}
+              >
+                <ArrowBigDownDash className="w-5" />
+              </button>
+              <button
+                className="bg-red-700 text-white custom-wrt-dlt"
+                onClick={() => deleteTask(index)}
+              >
+                <Trash2Icon className="w-5" />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+TaskItems.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      checked: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+  setTasks: PropTypes.func.isRequired,
+};
